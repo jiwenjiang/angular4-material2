3d7dcf863d76504fb02b2f782575ff5935804a5a
ji.wenjiang <513755565@qq.com>
2017-09-16 18:25:57 +0800

    add fork

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import {LoginComponent} from './core/login/login.component';
 import {NotFoundComponent} from './core/not-found/not-found.component';
 import {Auth} from './core/login/login.Auth';
 
-
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'stones', loadChildren: './stones/stones.module#StonesModule', canActivate: [Auth]},
