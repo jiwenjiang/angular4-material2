2574f83b591a7dc907f0e51f8fc5aba6839fc24c
ji.wenjiang <513755565@qq.com>
2017-09-16 15:44:17 +0800

    test pre-commit again

--- src/app/stones/stones-top/stones-top.component.ts
+++ src/app/stones/stones-top/stones-top.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stones-top',
-  templateUrl: "./stones-top.component.html",
+  templateUrl: './stones-top.component.html',
   styleUrls: ['./stones-top.component.less']
 })
 export class StonesTopComponent implements OnInit {
