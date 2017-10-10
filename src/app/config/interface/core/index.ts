03379aa56099a13340dc8c0b4831f6b54237a19a
513755565@qq.com <513755565@qq.com>
2017-10-10 20:43:00 +0800

    add interface for project

---
+++ src/app/config/interface/core/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by j_bleach on 2017/10/10.
+ */
+
+export interface Menus {
+  icon: string;
+  link: string;
+  text: any;
+  id: number;
+}
---
+++ src/app/config/interface/stones/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by j_bleach on 2017/10/10.
+ */
+
+export interface CardModel {
+  comment: string;
+  count: string;
+  date: string;
+  desc: string;
+  id: string;
+  like: string;
+  name: string;
+  role: string;
+  type: string;
+  url: string;
+}
